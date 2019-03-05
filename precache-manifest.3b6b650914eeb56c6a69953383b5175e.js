164ea50d48115eb026b7cb92f957ad98c4a6be22
rajju <vajrachara@yahoo.com>
2019-03-05 04:40:50 +0000

    Updates

---
+++ precache-manifest.3b6b650914eeb56c6a69953383b5175e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "df9ba67d45c9ec33fa5f",
+    "url": "/react-bootstrap-website/static/js/runtime~main.df9ba67d.js"
+  },
+  {
+    "revision": "16cfe4fa77cb3d96b985",
+    "url": "/react-bootstrap-website/static/js/main.16cfe4fa.chunk.js"
+  },
+  {
+    "revision": "c054ddcb0449519acc6c",
+    "url": "/react-bootstrap-website/static/js/2.c054ddcb.chunk.js"
+  },
+  {
+    "revision": "16cfe4fa77cb3d96b985",
+    "url": "/react-bootstrap-website/static/css/main.8b8b9736.chunk.css"
+  },
+  {
+    "revision": "c054ddcb0449519acc6c",
+    "url": "/react-bootstrap-website/static/css/2.fc86d3ac.chunk.css"
+  },
+  {
+    "revision": "dc219b410c2c9f2ffa54fad9d93afda6",
+    "url": "/react-bootstrap-website/index.html"
+  }
+];
